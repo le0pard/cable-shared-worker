e0cfefa736c616ead7d13a1f9855220d19a78dfb
Alexey Vasiliev <leopard.not.a@gmail.com>
2021-12-12 21:24:54 +0200

    support anycable

--- packages/worker/src/cables/anycable.js
+++ packages/worker/src/cables/anycable.js
@@ -1,3 +1,7 @@
+import {
+  WEBSOCKET_MESSAGE_COMMAND
+} from 'cable-shared/constants'
+
 export const initAnycableAPI = (api) => {
   let websocketConnection = null
   let portReceiverMapping = {}
@@ -10,13 +14,66 @@ export const initAnycableAPI = (api) => {
       })
     ),
     subscribeTo: ({port, portID, id, channel, params = {}}) => {
+      websocketConnection.subscribeTo(channel, params).then((subscriptionChannel) => {
+        subscriptionChannel.on('message', (data) => {
+          port.postMessage({command: WEBSOCKET_MESSAGE_COMMAND, data, id})
+        })
 
+        portReceiverMapping = {
+          ...portReceiverMapping,
+          [portID]: {
+            ...(portReceiverMapping[portID] || {}),
+            [id]: subscriptionChannel
+          }
+        }
+      })
     },
     unsubscribeFrom: (portID, id) => {
+      if (portReceiverMapping[portID] && portReceiverMapping[portID][id]) {
+        portReceiverMapping[portID][id].disconnect()
 
+        portReceiverMapping = Object.keys(portReceiverMapping).reduce((aggPorts, portKey) => {
+          const subsMap = Object.keys(portReceiverMapping[portKey]).reduce((aggSub, keySub) => {
+            if (portKey === portID && keySub === id) {
+              return aggSub
+            }
+            return {
+              ...aggSub,
+              [keySub]: portReceiverMapping[portKey][keySub]
+            }
+          }, {})
+
+          if (Object.keys(subsMap).length > 0) {
+            return {
+              ...aggPorts,
+              [portKey]: subsMap
+            }
+          }
+
+          return aggPorts
+        }, {})
+      }
     },
     unsubscribeAllFromPort: (portID) => {
+      if (portReceiverMapping[portID]) {
 
+        portReceiverMapping = Object.keys(portReceiverMapping).reduce((aggPorts, portKey) => {
+          if (portKey === portID) {
+            Object.keys(portReceiverMapping[portKey]).forEach((keySub) => {
+              if (portReceiverMapping[portKey][keySub]) {
+                portReceiverMapping[portKey][keySub].disconnect()
+              }
+            })
+
+            return aggPorts
+          }
+
+          return {
+            ...aggPorts,
+            [portKey]: portReceiverMapping[portKey]
+          }
+        }, {})
+      }
     },
     destroyCable: () => (
       new Promise((resolve) => {
