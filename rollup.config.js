4b46af405338b5bb5e04fafd48d4751504cc83f5
Alexey Vasiliev <leopard.not.a@gmail.com>
2023-05-18 19:24:27 +0300

    add source maps, fix eslint

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import alias from '@rollup/plugin-alias'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
-import { babel } from '@rollup/plugin-babel'
+import {babel} from '@rollup/plugin-babel'
 import terser from '@rollup/plugin-terser'
 // packages
 import webPkg from './packages/web/package.json'
